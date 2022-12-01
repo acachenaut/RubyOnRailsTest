1531692274e7437fe4372d9271ebbda8cfb2745e
Aitor Cachenaut <aitor.pilota@yahoo.fr>
2022-12-01 10:24:49 +0100

    Ajout de 2 test unitaires

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -4,4 +4,18 @@ class ArticleTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "the avg should equal 0" do
+    article = Article.new(title:"Test", body:"Titre d'un article test", status:"public")
+    assert article.noteAvg == 0
+  end
+
+  test "the avg should be between 0 and 5" do
+    article = Article.new(title:"test", body:"Titre d'un article test", status:"public")
+    article.save
+    note1 = article.notes.create(note:1)
+    note2 = article.notes.create(note:2)
+    note3 = article.notes.create(note:3)
+    note4 = article.notes.create(note:5)
+    assert (article.noteAvg > 0 && article.noteAvg <= 5)
+  end
 end
